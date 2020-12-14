6ca687c0d7d80875fd122b2646fc9830541ad10b
김민극 <rlaal567@gmail.com>
2020-12-14 14:48:58 +0900

    12/13 Update 차트 그래프 나오게 할 예정 또한 예상치 그래프 또한 그릴 예정 (10일정도?) 추가 UI는 다음기회에 이거 하고 물 먹기 APP 만들 예정

--- App2_0325/App2_0325.Android/MainActivity.cs
+++ App2_0325/App2_0325.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace App2_0325.Droid
 {
-    [Activity(Label = "종가예측", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "종가예측", Icon = "@drawable/jonga", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -21,7 +21,7 @@ namespace App2_0325.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-
+            OxyPlot.Xamarin.Forms.Platform.Android.PlotViewRenderer.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- App2_0325/App2_0325/MainPage.xaml.cs
+++ App2_0325/App2_0325/MainPage.xaml.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 using HtmlAgilityPack;
 using Xamarin.Forms;
 
+//drawing add
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.Annotations;
+
 namespace App2_0325
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
@@ -20,25 +28,37 @@ namespace App2_0325
         stock_[] stock = new stock_[_days];
         stock_v2[] stk_v2 = new stock_v2[_days];
         bool AI_Learn_flg = false;
+        bool chart_erase = false;
+        
+        //Add 0415 Skiasharp
+        SKPaint blackFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Black,
+            StrokeWidth = 5
+        };
 
         public MainPage()
         {
             InitializeComponent();
-
+            
         }
-        
+
         private void Button_Clicked_Search(object sender, EventArgs e)
         {
             sangjang sj = new sangjang();
+            ALL_Constants constants = new ALL_Constants();
 
             // enter 제거?
-            if (String.IsNullOrEmpty(DATA.Text)) { EDI1.Text = "ERR";return; }
+            if (String.IsNullOrEmpty(DATA.Text)) { EDI1.Text = "ERR"; return; }
             
             string search_data = DATA.Text.ToString();
             
             string jusik_code = "";
             EDI1.Text = "";
 
+            // 20/12/13            
+#if false
             for (int i = 0; i < 3721; i++)
             {
                 if (sj.jongmok[i].Equals(search_data))
@@ -49,50 +69,68 @@ namespace App2_0325
                 }
                 if (i == 3720) { EDI1.Text = "ERR"; return; }
             }
-
+#endif
+            for (int i = 0; i < constants.SJ_Number; i++)
+            {
+                if (sj.jongmok[i].Equals(search_data))
+                {
+                    EDI1.Text = sj.jongmok[i].ToString() + "\t";
+                    jusik_code = sj.company[i].ToString("D6");
+                    break;
+                }
+                if (i == (constants.SJ_Number - 1)) { EDI1.Text = "ERR"; return; }
+            }
 
             html_addr HTML_ADDR = new html_addr();
-
+            //EDI1.Text += " 60Day data analysis";
             //https://m.stock.naver.com/item/main.nhn#/stocks/084680/price
 
             //NEW
-            // //Page1 10days
-            // EDI1.Text = HTML_ADDR.html_HtmlDoc_page6(jusik_code, ref stock);
-            // //Page2 20days
-            // EDI1.Text += HTML_ADDR.html_HtmlDoc_page5(jusik_code, ref stock);
-            // //Page3 30days
-            // EDI1.Text += HTML_ADDR.html_HtmlDoc_page4(jusik_code, ref stock);
-            // //Page4 40days
-            // EDI1.Text += HTML_ADDR.html_HtmlDoc_page3(jusik_code, ref stock);
-            // //Page5 50days
-            // EDI1.Text += HTML_ADDR.html_HtmlDoc_page2(jusik_code, ref stock);
-            // //Page6 60days
-            // EDI1.Text += HTML_ADDR.html_HtmlDoc_page1(jusik_code, ref stock);
-
-            EDI1.Text = HTML_ADDR.html_HtmlDoc_page6_v2(jusik_code, ref stk_v2);
+            //Page1 10days
+            EDI1.Text = HTML_ADDR.html_HtmlDoc_page6(jusik_code, ref stock);
             //Page2 20days
-            EDI1.Text += HTML_ADDR.html_HtmlDoc_page5_v2(jusik_code, ref stk_v2);
+            EDI1.Text += HTML_ADDR.html_HtmlDoc_page5(jusik_code, ref stock);
             //Page3 30days
-            EDI1.Text += HTML_ADDR.html_HtmlDoc_page4_v2(jusik_code, ref stk_v2);
+            EDI1.Text += HTML_ADDR.html_HtmlDoc_page4(jusik_code, ref stock);
             //Page4 40days
-            EDI1.Text += HTML_ADDR.html_HtmlDoc_page3_v2(jusik_code, ref stk_v2);
+            EDI1.Text += HTML_ADDR.html_HtmlDoc_page3(jusik_code, ref stock);
             //Page5 50days
-            EDI1.Text += HTML_ADDR.html_HtmlDoc_page2_v2(jusik_code, ref stk_v2);
+            EDI1.Text += HTML_ADDR.html_HtmlDoc_page2(jusik_code, ref stock);
             //Page6 60days
-            EDI1.Text += HTML_ADDR.html_HtmlDoc_page1_v2(jusik_code, ref stk_v2);
+            EDI1.Text += HTML_ADDR.html_HtmlDoc_page1(jusik_code, ref stock);
+
+//              HTML_ADDR.html_HtmlDoc_page6_v2(jusik_code, ref stk_v2);
+////            //Page2 20days
+//              HTML_ADDR.html_HtmlDoc_page5_v2(jusik_code, ref stk_v2);
+////            //Page3 30days
+//                HTML_ADDR.html_HtmlDoc_page4_v2(jusik_code, ref stk_v2);
+////            //Page4 40days
+//                HTML_ADDR.html_HtmlDoc_page3_v2(jusik_code, ref stk_v2);
+////            //Page5 50days
+//                HTML_ADDR.html_HtmlDoc_page2_v2(jusik_code, ref stk_v2);
+////            //Page6 60days
+//                HTML_ADDR.html_HtmlDoc_page1_v2(jusik_code, ref stk_v2);
+
+
+            //SKPaint(Chart)
+            canvasView.InvalidateSurface();
 
             AI_Learn_flg = true;
+            chart_erase = true;
         }
 
         private void Button_Clicked_erase(object sender, EventArgs e)
         {
-            EDI1.Text = "CLEAR";
+            //EDI1.Text = "CLEAR";
             Lable1.Text = "CLEAR";
             AI_Learn_flg = false;
+            chart_erase = false;
             CLEAR stk_clr = new CLEAR();
 
             stk_clr.CLEAR_STOCK(ref stock,ref stk_v2);
-            
+
+            //SKPaint(Chart)
+            canvasView.InvalidateSurface();
         }
 
         private void Button_Clicked_Test(object sender, EventArgs e)
@@ -119,5 +157,126 @@ namespace App2_0325
             Learn_Result = BP.BP_START_STOCK_VERSION2(ref stk_v2);
             Lable1.Text = "종가 예측: " + (int)Learn_Result;
         }
+
+        private void canvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.White);
+            
+            int width = e.Info.Width;
+            int height = e.Info.Height;
+
+            SKBitmap sKBitmap = new SKBitmap(width, height);
+
+            SKPoint[] points = new SKPoint[GG._days];
+
+            int new_width = width / GG._days;
+            //int new_height = height
+
+            //for (int i = GG._days - 1; i > 0; i--)
+            //{
+            //    points[i] = new SKPoint(i* new_width, stk_v2[i].s_date);
+            //} 
+
+            // 20/12/14 code add
+            // add for High Price to Height 
+            int temp_height = 0;
+            for (int i = 0; i < GG._days; i++ )
+            {
+                if (temp_height < stk_v2[i].s_dhp_int )
+                {
+                    temp_height = stk_v2[i].s_dhp_int;
+                }
+            }
+            //add for Low Price to Height
+            int temp_height_low = stk_v2[0].s_dlp_int;
+            for (int i = 0; i < GG._days; i++)
+            {
+                if (temp_height_low > stk_v2[i].s_dlp_int)
+                {
+                    temp_height_low = stk_v2[i].s_dlp_int;
+                }
+            } 
+#if false
+            for (int i = 0; i < GG._days; i++)
+            {
+                float new_height = (stk_v2[i].s_dcp_int / (float)stk_v2[i].s_dhp_int) * height;
+                points[i] = new SKPoint(i * new_width, new_height);
+            }
+#endif            
+            for (int i = 0; i < GG._days; i++)
+            {
+                float new_height = (float)stk_v2[i].s_dcp_int;
+                points[i] = new SKPoint(i, new_height);
+            }
+
+            for (int i = 1; i < GG._days; i++)
+            {
+                //canvas
+                
+                canvas.DrawLine(points[i - 1], points[i], blackFillPaint);
+            }
+
+            Point[] point_oxy = new Point[GG._days];
+            
+
+             //OxyPlot
+             var model = new PlotModel
+             {
+                 Title = "Line Title",
+                 PlotType = PlotType.XY
+             };
+            
+             //X
+             model.Axes.Add(new LinearAxis
+             {
+                 Title = "Date",
+                 Position = AxisPosition.Bottom
+             });
+            
+             //Y
+             model.Axes.Add(new LinearAxis
+             {
+                 Title = "Close Price",
+                 Position = AxisPosition.Left
+             });
+            
+             var Points_Oxy = new List<DataPoint>();
+
+            //CreateLine();
+
+            var series2 = new OxyPlot.Series.LineSeries
+            {
+                LineStyle = LineStyle.None,
+                MarkerFill = OxyColors.Transparent,
+                MarkerStroke = OxyColors.Black,
+                MarkerStrokeThickness = 2
+            };
+
+
+            for (int i = 0; i < GG._days; i++)
+            {
+                series2.Points.Add(new DataPoint(stk_v2[i].s_date, stk_v2[i].s_dcp_int/100));
+            }
+            series2.Smooth = true;
+            model.Series.Add(series2);
+            
+
+        }
+
+        private void CreateLine()
+        {
+            var myModel = new PlotModel { Title = "Example 2" };
+            myModel.Series.Add
+                (
+                new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)")
+                );
+
+            this.PlotChart.Model = myModel;
+            
+        }
+
     }
 }
